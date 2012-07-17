4aa5fc9551102e2da6d7a5995704f99f0da48e14
Gabriel Horner <gabriel.horner@gmail.com>
2012-07-16 20:48:40 -0400

    print matches with table

--- src/rubydoc/core.clj
+++ src/rubydoc/core.clj
@@ -1,14 +1,21 @@
 (ns rubydoc.core
   (:require [clj-yaml.core :as yaml]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [table.core]))
 
-(declare rows)
+(declare rows print-matches)
 
 (defn rubydoc [str-or-regex]
   (let [matches? (if (instance? java.util.regex.Pattern str-or-regex)
                    #(re-find str-or-regex (str %))
                    #(.contains (str %) (str str-or-regex)))]
-    (filter #(matches? (:ruby %)) @rows)))
+    (print-matches
+      (filter #(matches? (:ruby %)) @rows))))
+
+(defn- print-matches [matches]
+  (if (= 1 (count matches))
+    (table.core/table (first matches))
+    (table.core/table matches)))
 
 (def ^:private rows
   (let [dir (-> (ClassLoader/getSystemResource *file*) io/file .getParent)]
