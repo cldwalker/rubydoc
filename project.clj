a19219813e338c1207138565b17c98acb4fed393
Gabriel Horner <gabriel.horner@gmail.com>
2012-07-24 18:00:07 -0400

    reference non-broken table version

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "https://en.wikipedia.org/wiki/MIT_License"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-yaml "0.4.0"]
-                 [table "0.3.1"]])
+                 [table "0.3.2"]])
